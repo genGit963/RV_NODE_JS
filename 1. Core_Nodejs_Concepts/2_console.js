cb89f6f8b4c3d16e6b565ad04b31b476d4e6681b
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-08 00:10:57 +0545

    2_console.js

--- 1. Core_Nodejs_Concepts/2_console.js
+++ 1. Core_Nodejs_Concepts/2_console.js
@@ -4,30 +4,30 @@
  * Class: Console
         new Console(stdout[, stderr][, ignoreErrors])
         new Console(options)
-        console.assert(value[, ...message])
-        console.clear()
-        console.count([label])
-        console.countReset([label])
-        console.debug(data[, ...args])
-        console.dir(obj[, options])
-        console.dirxml(...data)
-        console.error([data][, ...args])
-        console.group([...label])
-        console.groupCollapsed()
-        console.groupEnd()
-        console.info([data][, ...args])
-        console.log([data][, ...args])
-        console.table(tabularData[, properties])
-        console.time([label])
-        console.timeEnd([label])
-        console.timeLog([label][, ...data])
-        console.trace([message][, ...args])
-        console.warn([data][, ...args])
+            console.assert(value[, ...message])
+            console.clear()
+            console.count([label])
+            console.countReset([label])
+            console.debug(data[, ...args])
+            console.dir(obj[, options])
+            console.dirxml(...data)
+            console.error([data][, ...args])
+            console.group([...label])
+            console.groupCollapsed()
+            console.groupEnd()
+            console.info([data][, ...args])
+            console.log([data][, ...args])
+            console.table(tabularData[, properties])
+            console.time([label])
+            console.timeEnd([label])
+            console.timeLog([label][, ...data])
+            console.trace([message][, ...args])
+            console.warn([data][, ...args])
 
 Inspector only methods
-        console.profile([label])
-        console.profileEnd([label])
-        console.timeStamp([label])
+            console.profile([label])
+            console.profileEnd([label])
+            console.timeStamp([label])
  */
 
 /*
@@ -42,6 +42,7 @@ Inspector only methods
             to process.stdout and process.stderr. 
             The global console can be used without calling require('node:console').
  */
+
 console.log("hello world");
 // Prints: hello world, to stdout
 console.log("hello %s", "world");
@@ -57,6 +58,35 @@ console.error(new Error("Whoops, something bad happened"));
 //     at evalScript (node:internal/process/execution:76:60)
 //     at node:internal/main/eval_string:23:3
 
-const name = "Will Robinson";
-console.warn(`Danger ${name}! Danger!`);
+const name1 = "Will Robinson";
+console.warn(`Danger ${name1}! Danger!`);
 // Prints: Danger Will Robinson! Danger!, to stderr
+
+// const output = createWriteStream("./stdout.log");
+// const errorOutput = createWriteStream("./stderr.log");
+// defining our own logger
+// const logger = new Console({ stdout: output, stderr: errorOutput });
+// use it like console
+// const count = 5;
+// logger.log("count: %d", count);
+// In stdout.log: count 5
+
+console.assert(true, "this will assert");
+// console.clear()
+// console.count([label])
+// console.countReset([label])
+// console.debug(data[, ...args])
+// console.dir(obj[, options])
+// console.dirxml(...data)
+// console.error([data][, ...args])
+// console.group([...label])
+// console.groupCollapsed()
+// console.groupEnd()
+// console.info([data][, ...args])
+// console.log([data][, ...args])
+// console.table(tabularData[, properties])
+// console.time([label])
+// console.timeEnd([label])
+// console.timeLog([label][, ...data])
+// console.trace([message][, ...args])
+// console.warn([data][, ...args])
