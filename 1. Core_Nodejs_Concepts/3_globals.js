7b2a1d79b2c831beb6a16c399a7724116a2649f2
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-08 21:03:14 +0545

    3_globals.js

--- 1. Core_Nodejs_Concepts/3_globals.js
+++ 1. Core_Nodejs_Concepts/3_globals.js
@@ -1,5 +1,6 @@
 //         3. Globals – Understand Node.js global objects (like `__dirname`, `process`, etc.).
 
 console.log("--dirname", __dirname);
+console.log("--filename", __filename);
 
 console.log(Object);
