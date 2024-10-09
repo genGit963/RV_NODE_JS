ebf91c693e0c5f8bfa0e6bd9c5ce9f0ba345bfb3
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-09 12:55:27 +0545

    test.js

--- 1. Core_Nodejs_Concepts/test.js
+++ 1. Core_Nodejs_Concepts/test.js
@@ -6,5 +6,17 @@ emitter.on("event", () => {
   console.log({ data: "Here will be data" });
 });
 
+// Arguments taker Listener
+emitter.on("event_with_args", (arg1, arg2) => {
+  if (arg1 !== arg2) {
+    console.log("Given inputs are not equal !!");
+  } else {
+    console.log("Given inputs are equal");
+  }
+});
+
 // Emit an event
 emitter.emit("event");
+
+// Emit args event
+emitter.emit("event_with_args", 2, 3);
